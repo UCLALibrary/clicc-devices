15c97c595b8d34ef865903e6d48f92ec3d64031f
Zoe Tucker <zoetucker@library.ucla.edu>
2025-08-19 10:13:15 -0700

    SYS-1967: initial model setup and related config

--- clicc_devices/models.py
+++ clicc_devices/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Set(models.Model):
+    alma_set_id = models.CharField(
+        max_length=255, unique=True
+    )  # Alma set ID character limit is 255
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=100)
+    type = models.ForeignKey("ItemType", related_name="sets", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.unit}) - {self.type}"
+
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    set = models.ForeignKey(Set, related_name="items", on_delete=models.CASCADE)
+    barcode = models.CharField(max_length=128)  # Alma barcodes character limit is 128
+    retrieved_datetime = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.barcode} ({self.set.unit})"
